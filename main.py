8b96f8db2fcd7b5a6e1ce370e52dd9b0eae5e92f
julesgoz <julesgozlan@gmail.com>
2022-07-24 13:35:09 -0400

    Establish Spreadsheet Connection

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import csv
-
+from unicodedata import category
+import gspread
 # MONTH = "JUNE"
 
-file = "python-spending-tracker\docs\Discover-Statement-20220720.csv"
+file = r"C:\Users\Jules\Desktop\Python Projects\PythonFinances\python-spending-tracker\docs\Discover-Statement-20220720.csv"
 
 with open(file, mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
         print(row)
+
+sa = gspread.service_account()
+sh = sa.open("Personal Finances")
