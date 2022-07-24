8819ef7636db67b4c37e2d89ee1bd9d17cd5244e
julesgoz <julesgozlan@gmail.com>
2022-07-24 15:08:12 -0400

    added chase bank capabilities

--- main.py
+++ main.py
@@ -3,14 +3,7 @@ import gspread
 import time
 
 
-def discoverFin(file):
-    transactions = {'january': [], 'february': [], 'march': [],
-                    'april': [], 'may': [], 'june': [], 'july': [], 'august': [],
-                    'september': [], 'october': [], 'november': [], 'december': []}
-    months = {'01': 'january', '02': 'february', '03': 'march',
-                    '04': 'april', '05': 'may', '06': 'june', '07': 'july', '08': 'august',
-                    '09': 'september', '10': 'october', '11': 'november', '12': 'december'}
-
+def discoverFin(file, transactions):
     with open(file, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
@@ -19,8 +12,38 @@ def discoverFin(file):
             date = row[0]
             month = months[date[:2]]
             name = row[2]
-            amount = float(row[3])
+            amount = -float(row[3])
             category = row[4]
+            if category == "Payments and Credits" or category == "Awards and Rebate Credits":
+                continue
+            transaction = (date, name, category, amount)
+            transactions[month].append(transaction)
+        return transactions
+
+
+def findCategory(name):
+    categories = {'Montrose': '*RENT & BILLS*', 'VENMO': '*RENT & BILLS*', 'COINBASE': 'Crypto', 'AMZN': 'Merchandise', 'Amazon': 'Merchandise', 'DUKEENERGY': '*RENT & BILLS*',
+                  'DISCOVER': 'IGNORE'}
+    for i in range(1, len(name)):
+        if name[0:i] in categories:
+            return categories[name[0:i]]
+    return 'other'
+
+
+def chaseFin(file, transactions):
+    categories = {}
+    with open(file, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if row[0] == 'Details':
+                continue
+            date = row[1]
+            month = months[date[:2]]
+            name = row[2]
+            amount = float(row[3])
+            category = findCategory(name)
+            if row[2][:8] == 'Montrose':
+                category = '*RENT & BILLS*'
             if category == "Payments and Credits" or category == "Awards and Rebate Credits":
                 continue
             transaction = (date, name, category, amount)
@@ -30,17 +53,30 @@ def discoverFin(file):
 
 def main():
     # get csv data from bank generate file
-    data = r"C:\Users\Jules\Desktop\Python Projects\PythonFinances\python-spending-tracker\docs\Discover-2022-YearToDateSummary.csv"
+    discoverData = r"C:\Users\Jules\Desktop\Python Projects\PythonFinances\python-spending-tracker\docs\Discover-2022-YearToDateSummary.csv"
+    chaseData = r"C:\Users\Jules\Desktop\Python Projects\PythonFinances\python-spending-tracker\docs\Chase7438_Activity_20220724.CSV"
 
     # open connection to google spreadsheet
     sa = gspread.service_account()
     sh = sa.open("Personal Finances")
 
     # get transaction data organized
-    transactions = discoverFin(data)
+    global months
+    months = {'01': 'january', '02': 'february', '03': 'march',
+                    '04': 'april', '05': 'may', '06': 'june', '07': 'july', '08': 'august',
+                    '09': 'september', '10': 'october', '11': 'november', '12': 'december'}
+
+    transactions = {'january': [], 'february': [], 'march': [],
+                    'april': [], 'may': [], 'june': [], 'july': [], 'august': [],
+                    'september': [], 'october': [], 'november': [], 'december': []}
+    transactions = discoverFin(discoverData, transactions)
+    transactions = chaseFin(chaseData, transactions)
+
     for month in transactions.keys():
         wks = sh.worksheet(f"{month}")
+        wks.delete_rows(7, 150)
         rows = transactions[month]
+        rows = sorted(rows, key=lambda row: row[0])
         # insert data into spreadsheet
         wks.insert_rows(rows, 7)
         time.sleep(2)
